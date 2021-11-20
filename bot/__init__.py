a43cfaeb16ff76a608db1939c9e0ce5e2d2116b0
zoredeaxx <87483466+zoredeaxx@users.noreply.github.com>
2021-11-20 19:53:02 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -52,7 +52,7 @@ async def download(event):
             id_hex = hex(msg.id)[2:]
             id = f"{id_hex}/{get_file_name(msg)}"
             bot_url = f"t.me/{username_bot}?start={id_hex}"
-            await event.reply(f"Link to download file: \n\🖥 : {Config.DOMAIN}/{id}\n\📥 : {bot_url}")
+            await event.reply(f"Link to download file: \n\n📎 : {Config.DOMAIN}/{id}\n\n🤖 : {bot_url}")
             return
 
         elif id_msg := re.search("/start (.*)", event.raw_text ):
@@ -73,16 +73,16 @@ async def download(event):
                         forward = await file.forward_to(event.chat_id)
                         id_name = f"{id_hex}/{get_file_name(msg)}"
                         bot_url = f"t.me/{username_bot}?start={id_hex}"
-                        forward_reply = await forward.reply(f"will be deleted in 30 seconds. \n\n🖥 : {Config.DOMAIN}/{id_name}\n\n📥 : {bot_url}",link_preview=False)
-                        await asyncio.sleep(16)
-                        await forward_reply.edit(f"will be deleted in 15 seconds. \n\n🖥 : {Config.DOMAIN}/{id_name}\n\n📥 : {bot_url}")
-                        await asyncio.sleep(15)
+                        forward_reply = await forward.reply(f"will be deleted in 21 seconds. \n\n📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}",link_preview=False)
+                        await asyncio.sleep(12)
+                        await forward_reply.edit(f"will be deleted in 10 seconds. \n\n📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}")
+                        await asyncio.sleep(10)
                         await forward.delete()
-                        await forward_reply.edit(f"🖥 : {Config.DOMAIN}/{id_name}\n\n📥 : {bot_url}",link_preview=True)
+                        await forward_reply.edit(f"📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}",link_preview=True)
                 return
         
         if pv:
-            await event.reply("Send any file to get a Direct Download Link")
+            await event.reply("Send any file to get a link to download it")
         
 
     elif event.is_channel:
