bce627a172e4989b5d83b1f223a5b392976c683c
zoredeaxx <87483466+zoredeaxx@users.noreply.github.com>
2021-11-20 15:09:36 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    API_ID = int( os.getenv("api_id","1234") )
-    API_HASH = os.getenv("api_hash","21ab7cb0a453b5e60016dc7bbeb701cb")
-    CHANNEL = int( os.getenv("channel_files_chat_id","-1001249461809") )
-    CHANNEL_USERNAME = os.getenv("channel_username","UserLandapp")
-    TOKEN = os.getenv("token","xxxxx")
-    DOMAIN  = os.getenv("domain","https://newdlstar.herokuapp.com")
+    API_ID = int( os.getenv("api_id","7523379") )
+    API_HASH = os.getenv("api_hash","ce43762f206dc2a2eb115986fbe3b4a2")
+    CHANNEL = int( os.getenv("channel_files_chat_id","-1001523128336") )
+    CHANNEL_USERNAME = os.getenv("channel_username","zorstream")
+    TOKEN = os.getenv("token","2134090633:AAHPJ-E90WA-stTHsPLuMqKvqvTVIb-CGOQ")
+    DOMAIN  = os.getenv("domain","https://zorstreamz.herokuapp.com")
