df5b937d5a16816d453a28e23b107fc7f362f231
zoredeaxx <87483466+zoredeaxx@users.noreply.github.com>
2021-11-20 20:25:24 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -40,7 +40,7 @@ async def download(event):
                     participant = event.sender_id
                     ))
             except errors.UserNotParticipantError:
-                await event.reply(f"First join to our official channel to access the bot or get the newest news about the bot\n\n@{Config.CHANNEL_USERNAME}\n\nAfter that /start the bot aging.")
+                await event.reply(f"First join my support channel to access the bot.\n\n@{Config.CHANNEL_USERNAME}\n\nAfter that /start the bot again.")
                 return
         
         if event.file :
@@ -54,7 +54,7 @@ async def download(event):
             id_hex = hex(msg.id)[2:]
             id = f"{id_hex}/{get_file_name(msg)}"
             bot_url = f"t.me/{username_bot}?start={id_hex}"
-            await event.reply(f"Link to download file: \n\n📎 : {Config.DOMAIN}/{id}\n\n🤖 : {bot_url}")
+            await event.reply(f"Link to Download File: \n\🖥Stream : {Config.DOMAIN}/{id}\n\n📥File : {bot_url}")
             return
 
         elif id_msg := re.search("/start (.*)", event.raw_text ):
@@ -75,16 +75,16 @@ async def download(event):
                         forward = await file.forward_to(event.chat_id)
                         id_name = f"{id_hex}/{get_file_name(msg)}"
                         bot_url = f"t.me/{username_bot}?start={id_hex}"
-                        forward_reply = await forward.reply(f"will be deleted in 21 seconds. \n\n📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}",link_preview=False)
-                        await asyncio.sleep(12)
-                        await forward_reply.edit(f"will be deleted in 10 seconds. \n\n📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}")
-                        await asyncio.sleep(10)
+                        forward_reply = await forward.reply(f"The File will be deleted in 30 seconds. \n\n🖥Stream : {Config.DOMAIN}/{id_name}\n\📥File : {bot_url}",link_preview=False)
+                        await asyncio.sleep(16)
+                        await forward_reply.edit(f"will be deleted in 15 seconds. \n\n🖥Stream : {Config.DOMAIN}/{id_name}\n\n📥File : {bot_url}")
+                        await asyncio.sleep(15)
                         await forward.delete()
-                        await forward_reply.edit(f"📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}",link_preview=True)
+                        await forward_reply.edit(f"🖥Stream : {Config.DOMAIN}/{id_name}\n\n📥File : {bot_url}",link_preview=True)
                 return
         
         if pv:
-            await event.reply("Send any file to get a link to download it")
+            await event.reply("Send any file to get a Direct Download Link")
         
 
     elif event.is_channel:
