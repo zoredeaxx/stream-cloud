9ec6d1af3028b32560023eb2b71ae6db6bc975d2
zoredeaxx <87483466+zoredeaxx@users.noreply.github.com>
2021-12-23 12:57:15 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -5,5 +5,5 @@ class Config:
     API_HASH = os.getenv("api_hash","ce43762f206dc2a2eb115986fbe3b4a2")
     CHANNEL = int( os.getenv("channel_files_chat_id","-1001523128336") )
     CHANNEL_USERNAME = os.getenv("channel_username","zorstream")
-    TOKEN = os.getenv("token","2134090633:AAHPJ-E90WA-stTHsPLuMqKvqvTVIb-CGOQ")
-    DOMAIN  = os.getenv("domain","https://zorstreamz.herokuapp.com")
+    TOKEN = os.getenv("token","5051897880:AAFVmus0rFYwHMByh0kTIawHmcfcWUk_miE")
+    DOMAIN  = os.getenv("domain","https://doted-test-production.up.railway.app")
