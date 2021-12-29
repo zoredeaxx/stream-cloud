bbdf0d8012bf1fc4c7b955204db26e677144fceb
zoredeaxx <87483466+zoredeaxx@users.noreply.github.com>
2021-12-29 22:37:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn main:main --workers 2 --threads 4 --bind 0.0.0.0:$PORT --timeout 86400 --max-requests 1 --worker-class aiohttp.GunicornWebWorker & python -m bot
+gunicorn main:main --workers 2 --threads 4 --bind 0.0.0.0:$PORT --timeout 30 --max-requests 10 --worker-class aiohttp.GunicornWebWorker & python -m bot
